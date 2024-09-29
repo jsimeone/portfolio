2edd4ca9ada389c1ad9b76b2ad05cbd5cd6fe3cc
Joseph Simeone <jsimeone@hamilton.edu>
2024-09-29 15:03:22 -0400

    Change paths

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,25 @@
 import adapter from '@sveltejs/adapter-static';
-import { mdsvex } from 'mdsvex';
+import {mdsvex} from 'mdsvex';
 import {vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information projects adapters.
-		adapter: adapter(),
-		paths: {
-			// base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-		}
-	},
-	extensions: ['.svelte', '.md'],
-	preprocess: [
-		mdsvex({
-			extensions: ['.md']
-		}),
-		vitePreprocess()
-	]
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information projects adapters.
+        adapter: adapter(),
+        paths: {
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+        }
+    },
+    extensions: ['.svelte', '.md'],
+    preprocess: [
+        mdsvex({
+            extensions: ['.md']
+        }),
+        vitePreprocess()
+    ]
 };
 
 export default config;
