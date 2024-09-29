647fd46b60878613fa3fc414cb104f65f3b19e2f
Joseph Simeone <jsimeone@hamilton.edu>
2024-09-29 15:26:57 -0400

    please please please by sabrina carpenter

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,10 @@
 <script>
     export let title;
+    import {base} from "$app/paths";
 </script>
 
 <header>
-    <a href="/" class="flex items-center gap-1 pt-4 ps-4">
+    <a href="{base}/" class="flex items-center gap-1 pt-4 ps-4">
         <span class="material-symbols-outlined text-tertiary font-semibold text-xl">arrow_back_ios_new</span>
         <h1 class="text-primary text-3xl font-bold">{title}</h1>
     </a>
